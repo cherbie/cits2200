b3942a20968ab00cda7cb6e6c1b01751dd583cb0
clayton-herbst <herbsca1@gmail.com>
2019-04-04 08:59:39 +0800

    updates makefile and created LinkedListTest class

---
+++ lab4/ListLinkedTest.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import CITS2200.*;
+
+public class ListLinkedTest
+{
+	public static void main(String[] args)
+	{
+		if(args.length != 1)
+		{
+			System.out.println("1 integer argument required\n");
+			return;
+		}
+
+		
+	}
+}
