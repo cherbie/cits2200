afa879742b73c92ffa45d07f6d8366ccc416b9f2
Clayton <herbsca1@gmail.com>
2019-04-09 13:28:05 +0800

    BinTree.equals() function work

--- lab5/BinTree.java
+++ lab5/BinTree.java
@@ -3,20 +3,42 @@ import CITS2200.*;
 public class BinTree extends BinaryTree<E>
 {
     Object item;
-    BinaryTree b1;
-    BinaryTree b2;
+    BinaryTree leftTree;
+    BinaryTree rightTree;
 
     //CONSTRUCTORS
     public BinTree() { super(); }
     public BinTree(E item, BinaryTree<E> b1, BinaryTree<E> b2) {  super(item, b1, b2); }
 
     /**
-     * Should override the equals method that all objects have
-     * @return true if both binary trees have exactly the same structure, false otherwise.
+     * Tests whether the tree is equal to an Object
+     * where two trees are equal if either both trees are empty
+     * both trees contain equal items at the root (tested using the equals method of E)
+     * have equal left subtrees and equal right subtrees (recursively calling the equals method of BinaryTree.
+     * @override equals in class Object
+     * @return true if both trees are equal
+     * @param o Object
     **/
-    public void equals()
+    public abstract boolean equals(Object o)
     {
-
+        else if(!(o instanceof CITS2200.BinaryTree) || o == null)
+            return false; //Object is not a BinaryTree or child of binary tree.
+        else
+        {
+            //CHECK IF BOTH TREES ARE EMPTY (TRUE)
+            if(isEmpty() && o.isEmpty())
+                return true;
+            else
+            {
+                if(getItem() != o.getItem()) //CHECK IF BOTH TREES CONTAIN EQUAL ITEM AT THE ROOT
+                    return false;
+                if(!leftTree.equals( o.getLeft() )) //CHECK IF EQUAL LEFT SUBSTREES
+                    return false;
+                if(!rightTree.equals( o.getRight() )) //CHECK IF EQUAL RIGHT SUBTREES
+                    return false;
+                return true;
+            }
+        }
     }
 
     /**
---
+++ lab5/QueueIterator.java
@@ -0,0 +1,10 @@
+import CITS2200.*;
+
+public class QueueIterator<E> implements CITS2200.Iterator<E>
+{
+    
+    public QueueIterator()
+    {
+
+    }
+}
