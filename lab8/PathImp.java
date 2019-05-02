241e81a470cd65ca0a2309bc2437a95dd8754509
Clayton <herbsca1@gmail.com>
2019-05-02 09:53:35 +0800

    setting up project folder

---
+++ lab8/PathImp.java
@@ -0,0 +1,30 @@
+import CITS2200.*;
+
+public class PathImp implements CITS2200.Path
+{
+
+    //CONSTRUCTOR
+
+    public PathImp()
+    {
+        
+    }
+    //METHODS
+
+    /**
+     * @return int the weight of the minimum spanning tree, or -1 if no minimum spanning tree can be found.
+     */
+    public int getMinimumSpanningTree()
+    {
+
+    }
+
+    /**
+     * @return int[] of the diistances from the start vertex to each of the vertices in the graph.
+     */
+     public int getShortestPath(Graph G, int vertex)
+     {
+
+     }
+
+}
